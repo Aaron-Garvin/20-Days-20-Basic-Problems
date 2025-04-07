456c8602f630c8fa2853d3eed457ee4abdc6a89a
Aaron-Garvin <garvinaaron26@gmail.com>
2025-04-07 14:08:29 +0530

    Delete Count_even_odd_number_in_list/Ex1,py.py

--- Count_even_odd_number_in_list/Ex1,py.py
+++
@@ -1,8 +0,0 @@
-numbers = [10, 21, 4, 45, 66, 93, 1]
-
-even_count = [num for num in numbers if num % 2 == 0]
-
-odd_count = [num for num in numbers if num % 2 != 0]
-
-print("Even Numbers:", even_count)
-print("Odd Numbers:", odd_count)
